95b9475a972cab72d2501856a3567d5e80c82046
osso2k <ossoble817@gmail.com>
2026-01-18 08:58:27 +0300

    UI is proper difficult ehh

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,7 @@
 
 // import api from '../api/axios.ts';
 
-// import CreateTask from "./components/createTask"
+import CreateTask from "./components/createTask"
 // import TasksList from "./components/TasksList"
 
 // import Navbar from "./components/Navbar"
@@ -11,7 +11,7 @@ import { LuArchive } from "react-icons/lu";
 const App = () => {
   
   return (
-  <div className="flex min-h-screen w-full">
+  <div className="flex flex-col min-h-screen w-full">
     <div className="flex flex-col w-full">
       <div className="flex  mt-24 ml-[25%] ">
         <LuArchive className="text-3xl my-auto text-blue-400" />
@@ -19,7 +19,7 @@ const App = () => {
       </div>
       <p className="ml-[26.5%] font-serif text-3xs text-zinc-600">Start working on your future one day at a time!</p>
     </div>
-
+    <CreateTask />
 
   </div>
   )
--- front/src/components/CreateTask.tsx
+++ front/src/components/CreateTask.tsx
@@ -1,9 +1,18 @@
-
+import { GrFormAdd } from "react-icons/gr";
 
 const createTask = () => {
   return (
-    <div>
-      
+    <div className="mt-10 w-[50%] mx-auto h-[20%] bg-amber-50 rounded-2xl">
+      <div className="grid grid-rows-2 gap-2">
+        <div className="order-1 flex mt-4 ml-5">
+            <GrFormAdd className="text-5xl text-blue-200" />
+            <h2 className="my-auto text-xl text-zinc-900">Add New Task</h2>
+        </div>
+        <div className="order-2">
+
+        </div>
+
+      </div>
     </div>
   )
 }
